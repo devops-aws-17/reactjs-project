fd6bf57f59ab0e14c6234c017a3b42e53228ccd9
EC2 Default User <ec2-user@ip-10-0-1-10.ap-south-1.compute.internal>
2026-03-19 07:00:57 +0000

    modified files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 import CreateEmployeeComponent from './components/CreateEmployeeComponent';
-import UpdateEmployeeComponent from './components/UpdateEmployeeComponent';
 import ViewEmployeeComponent from './components/ViewEmployeeComponent';
+
+// Remove UpdateEmployeeComponent import for now since it's unused
+// import UpdateEmployeeComponent from './components/UpdateEmployeeComponent';
 
 function App() {
   return (
     <div>
-        <Router>
-              <HeaderComponent />
-                <div className="container">
-                    <Switch> 
-                          <Route path = "/" exact component = {ListEmployeeComponent}></Route>
-                          <Route path = "/employees" component = {ListEmployeeComponent}></Route>
-                          <Route path = "/add-employee/:id" component = {CreateEmployeeComponent}></Route>
-                          <Route path = "/view-employee/:id" component = {ViewEmployeeComponent}></Route>
-                          {/* <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route> */}
-                    </Switch>
-                </div>
-              <FooterComponent />
-        </Router>
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={ListEmployeeComponent}></Route>
+            <Route path="/employees" component={ListEmployeeComponent}></Route>
+            <Route path="/add-employee/:id" component={CreateEmployeeComponent}></Route>
+            <Route path="/view-employee/:id" component={ViewEmployeeComponent}></Route>
+            {/* Uncomment when using update route */}
+            {/* <Route path="/update-employee/:id" component={UpdateEmployeeComponent}></Route> */}
+          </Switch>
+        </div>
+        <FooterComponent />
+      </Router>
     </div>
-    
   );
 }
 
--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,28 +1,31 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://43.205.239.227:8080/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "http://10.0.3.14:8080/api/v1/employees";
 
 class EmployeeService {
 
-    getEmployees(){
+    getEmployees() {
         return axios.get(EMPLOYEE_API_BASE_URL);
     }
 
-    createEmployee(employee){
+    createEmployee(employee) {
         return axios.post(EMPLOYEE_API_BASE_URL, employee);
     }
 
-    getEmployeeById(employeeId){
-        return axios.get(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+    getEmployeeById(employeeId) {
+        return axios.get(`${EMPLOYEE_API_BASE_URL}/${employeeId}`);
     }
 
-    updateEmployee(employee, employeeId){
-        return axios.put(EMPLOYEE_API_BASE_URL + '/' + employeeId, employee);
+    updateEmployee(employee, employeeId) {
+        return axios.put(`${EMPLOYEE_API_BASE_URL}/${employeeId}`, employee);
     }
 
-    deleteEmployee(employeeId){
-        return axios.delete(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+    deleteEmployee(employeeId) {
+        return axios.delete(`${EMPLOYEE_API_BASE_URL}/${employeeId}`);
     }
 }
 
-export default new EmployeeService()
+// Named instance to satisfy ESLint
+const employeeService = new EmployeeService();
+
+export default employeeService;
